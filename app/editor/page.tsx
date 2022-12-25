8b9167a6da81bfa95772a91a370e304652538603
Miklos Danka <mdanka@users.noreply.github.com>
2022-12-25 17:15:14 +0100

    Add tailwind

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -14,6 +14,9 @@ export default function Page() {
 
     return (
         <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
             <EditorContent editor={editor} />
         </div>
     )
